a1be0b1a0791acc2a136cfc10a04a216ff6acaa0
yuryeuceda <euceda.yury@gmail.com>
2024-10-15 22:05:38 -0600

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,5 +5,5 @@ echo $PASS >> /tmp/.auth
 
 scp /tmp/.auth remote_user@production_host:/tmp/.auth
 scp /pipeline/jenkins/deploy/publish.sh remote_user@production_host:/tmp/publish.sh
-ssh remote_user@production_host "/tmp/publish.sh"
+ssh remote_user@production_host "sh /tmp/publish.sh"
 #/tmp/publish.sh
