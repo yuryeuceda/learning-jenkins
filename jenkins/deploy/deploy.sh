bb1077fdb684053b18cdb9492ee588a72386ff4b
yuryeuceda <euceda.yury@gmail.com>
2024-10-21 23:20:58 -0600

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,7 +5,6 @@ echo $PASS >> /tmp/.auth
 
 scp /tmp/.auth remote_user@production_host:.auth
 scp ./jenkins/deploy/publish.sh remote_user@production_host:publish.sh
-
 scp ./jenkins/maven/docker-compose.yml remote_user@production_host:docker-compose.yml
 
 ssh remote_user@production_host "sh publish.sh"
