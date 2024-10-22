852c344f2150f7cd90cfc345eaf996bbdf175103
yuryeuceda <euceda.yury@gmail.com>
2024-10-21 23:30:46 -0600

    Update publish.sh

--- jenkins/deploy/publish.sh
+++ jenkins/deploy/publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export IMAGE=$(sed -n '1p' .auth)
-export TAG=$(sed -n '2p' .auth)
-export PASS=$(sed -n '3p' .auth)
+export IMAGE=$(sed -n '1p' auth)
+export TAG=$(sed -n '2p' auth)
+export PASS=$(sed -n '3p' auth)
 
 docker login -u yuryeuceda -p $PASS
 docker-compose up -d
