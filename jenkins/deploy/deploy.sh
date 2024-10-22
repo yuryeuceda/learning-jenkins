7a158602e114eec1c58eb891f8e7c95d882f296e
yuryeuceda <euceda.yury@gmail.com>
2024-10-21 23:22:56 -0600

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -6,9 +6,8 @@ echo $PASS >> /tmp/.auth
 scp /tmp/.auth remote_user@production_host:.auth
 scp ./jenkins/deploy/publish.sh remote_user@production_host:publish.sh
 scp ./jenkins/maven/docker-compose.yml remote_user@production_host:docker-compose.yml
+ssh remote_user@production_host "cd ~ && sh publish.sh"
 
-ssh remote_user@production_host "sh publish.sh"
 #/tmp/publish.sh
-
 #docker login -u yuryeuceda -p $PASS
 #cd /pipeline/jenkins/maven && docker-compose up -d
