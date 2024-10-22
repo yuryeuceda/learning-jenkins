3b16c5d173c9574c51f35d94999864db0de74da8
yuryeuceda <euceda.yury@gmail.com>
2024-10-21 23:37:23 -0600

    Update publish.sh

--- jenkins/deploy/publish.sh
+++ jenkins/deploy/publish.sh
@@ -4,5 +4,6 @@ export IMAGE=$(sed -n '1p' auth)
 export TAG=$(sed -n '2p' auth)
 export PASS=$(sed -n '3p' auth)
 
-docker login -u yuryeuceda -p $PASS
+#docker login -u yuryeuceda -p $PASS
+echo "$PASS" | docker login --username yuryeuceda --password-stdin
 docker-compose up -d
