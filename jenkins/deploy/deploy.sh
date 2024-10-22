f20c9c2b43fe6161343044d9390b4cb02461e832
yuryeuceda <euceda.yury@gmail.com>
2024-10-21 22:53:20 -0600

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,8 +4,8 @@ echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
 scp /tmp/.auth remote_user@production_host:/tmp/.auth
-scp ./jenkins/deploy/publish.sh remote_user@production_host:~/publish.sh
-ssh remote_user@production_host "sh ~/publish.sh"
+scp ./jenkins/deploy/publish.sh remote_user@production_host:publish.sh
+ssh remote_user@production_host "sh publish.sh"
 #/tmp/publish.sh
 
 #docker login -u yuryeuceda -p $PASS
