17d3bf0d3f848c1df0ab334fc02c24a5e5d2b6bd
yuryeuceda <euceda.yury@gmail.com>
2024-10-21 22:45:28 -0600

    Update test.sh

--- jenkins/test/test.sh
+++ jenkins/test/test.sh
@@ -5,4 +5,4 @@ echo '*** Running the tests ***'
 echo '*************************'
 
 #docker run -it --rm -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine mvn test
-docker run -it --rm -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
+docker run -i --rm -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
