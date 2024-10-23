07220d1d5dbe7ed7f0ca4e3f980804102b7fc377
yuryeuceda <euceda.yury@gmail.com>
2024-10-22 21:33:18 -0600

    Update publish.sh

--- jenkins/deploy/publish.sh
+++ jenkins/deploy/publish.sh
@@ -8,6 +8,7 @@ export PASS=$(sed -n '3p' auth)
 echo "Login into dockerhub"
 #docker login -u yuryeuceda -p $PASS
 #echo "$PASS" | docker login --username yuryeuceda --password-stdin
-echo "$PASS" | docker login -u 'yuryeuceda' --password-stdin
+#echo "$PASS" | docker login -u 'yuryeuceda' --password-stdin
+docker login -u 'yuryeuceda' -p "$PASS"
 echo "Composing"
 docker-compose up -d
