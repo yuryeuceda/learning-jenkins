584445697ee51edef9c41516e6dabbd6fd42d30a
Yury Euceda <euceda.yury@gmail.com>
2024-10-23 22:34:32 -0600

    Agregando un custom hook

--- java-app/src/main/java/com/mycompany/app/App.java
+++ java-app/src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello from Pipeline!";
+    private final String message = "Hello from Pipeline. Im sleepy!";
 
     public App() {}
 
--- java-app/src/test/java/com/mycompany/app/AppTest.java
+++ java-app/src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello from Pipeline!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello from Pipeline. Im sleepy!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
